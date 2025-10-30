6c1b52e6f3b72ccc03d80a1cc00a64efdb6650d6
Ital023 <contadoitalo911@gmail.com>
2025-10-30 18:48:00 -0300

    init: initial files

---
+++ src/test/java/io/github/ital023/SmartStock/SmartStockApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ital023.SmartStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
